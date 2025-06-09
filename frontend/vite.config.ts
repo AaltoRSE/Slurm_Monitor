8b65af638c3617bb294f65d9cd87f4badb486f21
Thomas Pfau <github@thomaspfau.de>
2025-06-09 14:18:26 +0300

    Trying relative path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,9 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dotenv from "dotenv";
-import { dirname, basename } from "node:path";
 
 // Load environment variables from .env file
 dotenv.config();
-
-const currentDir = dirname(fileURLToPath(import.meta.url)); // Current directory
-const parentDir = basename(dirname(currentDir)); // Parent's parent directory
 
 process.env.VITE_BASE_URL = process.env.VITE_BASE_URL || `./`;
 
